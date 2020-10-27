0e20b6d4ad50fe9cdb3000a75f967e6f8391bfcc
Vector254 <ochrist7@gmail.com>
2020-10-27 10:54:59 +0300

    register models

--- v_rate/admin.py
+++ v_rate/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, Projects, Rate
 
 # Register your models here.
+admin.site.register(Projects)
+admin.site.register(Profile)
+admin.site.register(Rate)
