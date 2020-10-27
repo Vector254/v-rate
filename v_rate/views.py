3dc7dcbcc0efe8bf1c48f107b1f79729065d4c40
Vector254 <ochrist7@gmail.com>
2020-10-27 11:19:40 +0300

    create registration and profile views

--- v_rate/views.py
+++ v_rate/views.py
@@ -6,3 +6,43 @@ from .models import Profile, Projects, Rate
 
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully!')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/registration_form.html',{'form':form})
+
+def profile(request):
+    projects = Profile.projects.all()
+    #user_profile = get_object_or_404(User, pk=pk)
+   
+    if request.method=='POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+           user_form.save() 
+           profile_form.save()
+           messages.success(request, f'Profile info updated successfully!')
+           return redirect('profile' )
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    params = {
+        'images':projects,
+        'user_form':user_form,
+        'profile_form':profile_form,
+        
+       
+    }
+   
+    return render(request, 'profile.html', params)
