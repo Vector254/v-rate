9fe01fcbd20d9079d451a13475dc4b3475c04922
Vector254 <ochrist7@gmail.com>
2020-10-28 07:57:17 +0300

    create urls for api requests

--- v_rate/urls.py
+++ v_rate/urls.py
@@ -15,6 +15,8 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search'),
     url(r'project/(\d+)',views.detail,name = 'details'),
     url(r'project/new/',views.create_post,name = 'create'),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
+    url(r'^api/profiles/$', views.ProfileList.as_view())
 
 ]
 if settings.DEBUG:
